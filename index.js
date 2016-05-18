2bc484f44f2adfaf12cc16010f1e2d6af1af66b8
thejsj <jorge.silva@thejsj.com>
2016-05-18 15:16:37 -0700

    Move promisify func to file

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const PrimusClient = require('@runnable/api-client/lib/external/primus-client')
 const Runnable = require('@runnable/api-client')
 
 const socketUtils = require('./lib/socket/utils.js')
+const promisifyClientModel = require('./lib/utils/promisify-client-model')
 
 // Parse ENVs and passed args
 const opts = require('./lib/utils/env-arg-parser')
@@ -33,30 +34,6 @@ const reqOpts = {
   headers: {
     'User-Agent': 'runnable-integration-test'
   }
-}
-
-const promisifyClientModel = (obj) => {
-  const hasProp = {}.hasOwnProperty;
-  for (var key in obj) {
-    ((key) => {
-      if (hasProp.call(obj, key + 'Async') !== false) {
-        return
-      }
-      if (typeof obj[key] === 'function') {
-        let myFunc = function () {
-          let results
-          return Promise.fromCallback((cb) => {
-            const args = [].slice.call(arguments)
-            args.push(cb)
-            results = obj[key].apply(obj, args)
-          })
-            .return(results)
-        }
-        obj[key + 'Async'] = myFunc
-      }
-    })(key)
-  }
-  return obj
 }
 
 before((done) => {
