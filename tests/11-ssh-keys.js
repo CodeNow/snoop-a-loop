d65ce9f21dd65137f951987118d904b1fb60ea1c
Myztiq <kahn@runnable.com>
2017-05-25 15:06:56 +0300

    Added tests for SSH KEYS!

---
+++ tests/11-ssh-keys.js
@@ -0,0 +1,32 @@
+'use strict'
+const sshKeys = require('../lib/ssh-keys/ssh-keys')
+
+module.exports = (config) => {
+  const opts = config.opts
+
+  describe('11. SSH Keys', function () {
+    if (opts.NO_SSH_KEYS) this.pending = true
+
+    before(() => {
+      console.log('Finding and deleting old generated ssh keys on github.')
+      return sshKeys.cleanupGithubKeys()
+    })
+
+    after(() => {
+      console.log('Finding and deleting generated ssh keys on github.')
+      return sshKeys.cleanupGithubKeys()
+    })
+
+    describe('create ssh key using api', () => {
+      it('should return successful', () => {
+        return sshKeys.createRunnableKey()
+      })
+    })
+
+    describe('create new instance requiring ssh keys', () => {
+      it('should build and start properly', () => {
+
+      })
+    })
+  })
+}
