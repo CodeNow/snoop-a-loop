855a64d2f7b1e4be44466532db58c762fdd478b5
Myztiq <kahn@runnable.com>
2017-05-28 14:34:27 +0300

    Added test which verifies that ssh keys are working end to end.

--- tests/11-ssh-keys.js
+++ tests/11-ssh-keys.js
@@ -1,10 +1,15 @@
 'use strict'
+const AuthenticatedRequest = require('../lib/request/authenticated-request.js')
+const common = require('../lib/utils/common')
 const expect = require('chai').expect
+const InstanceUtils = require('../lib/instance/util')
+const promisifyClientModel = require('../lib/utils/promisify-client-model')
 const sshKeys = require('../lib/ssh-keys/ssh-keys')
 require('chai').use(require('dirty-chai'))
 
 module.exports = (config) => {
   const opts = config.opts
+  const client = config.client
 
   describe('11. SSH Keys', function () {
     if (opts.NO_SSH_KEYS) this.pending = true
@@ -33,8 +38,48 @@ module.exports = (config) => {
     })
 
     describe('create new instance requiring ssh keys', () => {
+      let sshKeysInstance
+      before(function () {
+        this.timeout(50000)
+        const myParams = {
+          method: 'POST',
+          uri: opts.API_URL + '/docker-compose-cluster/',
+          json: {
+            repo: `${opts.GITHUB_USERNAME}/${opts.SNOOP_SSH_KEYS_REPO_NAME}`,
+            branch: 'master',
+            filePath: '/docker-compose.yml',
+            name: `ssh-key-test-${common.randInt}`,
+            githubId: opts.GITHUB_OAUTH_ID
+          }
+        }
+        return AuthenticatedRequest.apiRequest(myParams)
+          .then(() => {
+            const waitForInstance = () => {
+              return Promise.delay(500)
+                .then(() => {
+                  return client.fetchInstancesAsync({ githubUsername: opts.GITHUB_USERNAME })
+                })
+                .then((instances) => {
+                  const foundInstance = instances.models.find((instance) => {
+                    return instance.attrs.name === `ssh-key-test-${common.randInt}-web`
+                  })
+                  if (!foundInstance) {
+                    return waitForInstance()
+                  }
+                  console.log('Instance created', `ssh-key-test-${common.randInt}-web`)
+                  sshKeysInstance = promisifyClientModel(foundInstance)
+                })
+            }
+            return waitForInstance()
+          })
+      })
+
+      after(() => {
+        return sshKeysInstance.destroyAsync()
+      })
+
       it('should build and start properly', () => {
-        // TODO: implementation
+        return InstanceUtils.assertInstanceIsRunning(sshKeysInstance)
       })
     })
   })
