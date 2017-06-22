93542a49aeef7ada996228eea986a9f0ca269ecf
Myztiq <kahn@runnable.com>
2017-06-22 13:39:17 -0700

    Updated to work with multiple webhooks

--- tests/multiple-webhooks.js
+++ tests/multiple-webhooks.js
@@ -14,16 +14,16 @@ module.exports = (config) => {
     if (opts.NO_MULTIPLE_WEBHOOKS) this.pending = true
 
     describe('create multiple instances for multiple webhooks', () => {
-      let testInstance1
-      let testInstance2
+      let testInstances
       let testPath = 'multiple-webhooks'
       let serviceName = 'test1'
       let serviceName2 = 'test2'
       before(function () {
         this.timeout(50000)
+        testInstances = []
         const myParams = {
           method: 'POST',
-          uri: opts.API_URL + '/docker-compose-cluster/',
+          uri: opts.API_URL + '/docker-compose-cluster/multi',
           json: {
             repo: `${opts.GITHUB_USERNAME}/${opts.SNOOP_TESTS_REPO}`,
             branch: 'master',
@@ -34,47 +34,45 @@ module.exports = (config) => {
         }
         return AuthenticatedRequest.apiRequest(myParams)
           .then(() => {
-            const waitForInstance = (instanceName) => {
+            const waitForInstances = () => {
               return Promise.delay(500)
                 .then(() => {
                   return client.fetchInstancesAsync({ githubUsername: opts.GITHUB_USERNAME })
                 })
                 .then((instances) => {
-                  const foundInstance = instances.models.find((instance) => {
-                    return instance.attrs.name === instanceName
+                  let foundInstances = []
+                  let instanceName = `${testPath}-test-${common.randInt}-${serviceName}`
+                  let instanceName2 = `${testPath}-test-${common.randInt}-${serviceName2}`
+                  instances.models.forEach((instance) => {
+                    if (instance.attrs.name.indexOf(instanceName) > 0 || instance.attrs.name.indexOf(instanceName2) > 0) {
+                      foundInstances.push(instance)
+                    }
                   })
-                  if (!foundInstance) {
-                    return waitForInstance(instanceName)
+                  if (foundInstances.length !== 4) {
+                    return waitForInstances()
                   }
-                  console.log('Instance created', instanceName)
-                  return promisifyClientModel(foundInstance)
+                  console.log('All test instances created', foundInstances.map((instance) => {
+                    return instance.attrs.name
+                  }))
+                  testInstances = foundInstances.map((instance) => {
+                    return promisifyClientModel(instance)
+                  })
                 })
             }
-            return Promise.all([
-              waitForInstance(`${testPath}-test-${common.randInt}-${serviceName}`)
-                .then((instance) => {
-                  testInstance1 = instance
-                }),
-              waitForInstance(`${testPath}-test-${common.randInt}-${serviceName2}`)
-                .then((instance) => {
-                  testInstance2 = instance
-                })
-            ])
+            return waitForInstances()
           })
       })
 
       after(() => {
-        return Promise.all([
-          testInstance1.destroyAsync(),
-          testInstance2.destroyAsync()
-        ])
+        return Promise.each(testInstances, (instance) => {
+          return instance.destroyAsync()
+        })
       })
 
       it('should build and start properly for all instances', () => {
-        return Promise.all([
-          InstanceUtils.assertInstanceIsRunning(testInstance1),
-          InstanceUtils.assertInstanceIsRunning(testInstance2)
-        ])
+        return Promise.all(testInstances.forEach((instance) => {
+          return InstanceUtils.assertInstanceIsRunning(instance)
+        }))
       })
     })
   })
